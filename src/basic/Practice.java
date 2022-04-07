9dceecf001ff451bff084fab8b88266b3b950520
wasee112 <slcom.nl@gmail.com>
2022-04-06 22:49:06 -0400

    First

---
+++ src/basic/Practice.java
@@ -0,0 +1,24 @@
+package basic;
+
+public class Practice {
+	
+	static int salary = 4000;
+	
+	static double annualSalary = 48000;
+	
+	 double NetSalary = 48000 * .3;
+	 
+	 protected static boolean Paid = true;
+	 
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("My Monthly Salary is = " + Practice.salary);
+		
+		
+		
+	}
+
+}
---
+++ src/basic/Practice2.java
@@ -0,0 +1,23 @@
+package basic;
+
+public class Practice2 {
+	
+	
+	public static void main(String[] args) {
+		
+		Practice newobj = new Practice();
+		
+		
+		
+		System.out.println("Trying Modifier value." +"  My salary is " +  Practice.salary);
+		System.out.println(" Bringing up instance " + newobj.NetSalary);
+		
+	System.out.println("Salary paid = " + newobj.Paid);
+		
+	
+		
+		
+		
+	}
+
+}
